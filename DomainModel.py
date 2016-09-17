d368cea08a762d529aa033b45b9b0387100f1e4d
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2016-09-17 08:56:32 -0300

    Correção do Domain Model

--- DomainModel.py
+++ DomainModel.py
@@ -1,270 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import enum
 
-#pessoas_enderecos = Table('pessoas_enderecos', Base.metadata,
-#    Column('pessoa_id', Integer, ForeignKey('Pessoa.id')),
-#    Column('endereco_id', Integer, ForeignKey('Endereco.id'))
-#)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/CGP'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
 
-
-class Pessoa(Model):
-	__tablename__ = 'pessoas'
-	id = Column(Integer, primary_key = True)
-	nome = Column(String(200), unique=True)
-	matricula = Column(String(12), unique=True)
-	matriculaOrigem  = Column(String(12), unique=True)
-	identificacaoUnica = Column(String(12), unique=True)
-	tipoServidor = Column(Enum(TipoServidor))
-	situacaoServidor = Column(Enum(SituacaoServidor))
-	dataCadastroSiape = Column(DateTime)
-	dataNascimento = Column(DateTime)
-	estadoCivil  = Column(Enum(EstadoCivil))
-	dataPrimeiroEmprego = Column(DateTime)
-	nacionalidade = Column(String(200))
-	ufNascimento  = Column(Enum(UF))
-	nomeMae = Column(String(200))
-	nomePai = Column(String(200))
-	naturalidade = Column(String(200))
-	tipoSanguineo = Column(Enum(TipoSanguineo))
-	sexo  = Column(Enum(Sexo))
-	possuiDeficiencia = Column(Boolean)
-	TipoDeficiência = Column(String(200))
-	raca_cor = Column(String(200))
-	enderecos = relationship("Endereco", back_populates="pessoa")
-	telefones = relationship("Telefone", back_populates="pessoa")
-	email_Pessoal = Column(String(120), unique=True)
-	email_Institucional  = Column(String(120), unique=True)
-	rg_Numero = Column(String(200))
-	rg_OrgaoExpedidor = Column(String(200))
-	rg_UF = Column(Enum(UF))
-	rg_Emissao = Column(DateTime)
-	cpf = Column(String(200), unique=True)
-	pis_pased = Column(String(20))
-	tituloEleitor = Column(String(20))
-	tituloEleitor_UF = Column(Enum(UF))
-	tituloEleitor_Zona = Column(String(5))
-	tituloEleitor_Secao = Column(String(5))
-	tituloEleitor_Emissao = Column(DateTime)
-	certificadoMilitar = Column(String(20))
-	certificadoMilitar_Orgao = Column(String(200))
-	certificadoMilitar_Serie = Column(String(20))
-	passaporte = Column(String(20))
-	pagamento_Banco = Column(String(5))
-	pagamento_Agencia = Column(String(12))
-	pagamento_Conta = Column(String(12))
-	pagamento_TipoConta = Column(Enum(TipoContaBancaria))
-	cargos = relationship("Cargo", back_populates="pessoa")
-	funcoes = relationship("Funcao", back_populates="pessoa")
-	titulos = relationship("Titulos", back_populates="pessoa")
-	progressoes = relationship("Progressao", back_populates="pessoa")
-	
-	jornada = Column(Enum(JornadaTrabalho))
-	
-	def __init__(self,logradouro, numero, complemento, bairro, municipio, pais, estado, cep, pessoa)
-		self.logradouro = logradouro
-		self.numero = numero
-		self.complemento = complemento
-		self.bairro = bairro
-		self.municipio = municipio
-		self.pais = pais
-		self.estado = estado
-		self.cep = cep
-		self.pessoa = pessoa
-		
-	def __repr__(self):
-        return '<Pessoa %r>' % self.nome
-        	
-class ClasseNivel
-	__tablename__ = 'classesniveis'
-	id = Column(Integer, primary_key = True
-	classe = Column(String(5))
-	nivel = Column(Integer)
-	
-	def __init__(self,classe,nivel)
-		 self.classe = classe
-		 self.nivel = nivel
-	
-	def __repr__(self):
-        return '<Setor %r>' % self.nome
-
-class Progressao(Model):
-	id = Column(Integer, primary_key = True
-	classenivel_id = Column(Integer, ForeignKey('ClasseNivel.id'))
-	classenivel = relationship('ClasseNivel')
-	dataInicio = Column(DateTime)
-	dataTermino = Column(DateTime)
-	
-	pessoa_id = Column(Integer, ForeignKey('pessoa.id'))
-    pessoa = relationship('Pessoa',  backref=backref('progressoes', lazy='dynamic'))
-    
-    def __init__(self,classenivel,dataInicio,dataTermino,pessoa)
-		self.classenivel = classenivel
-		self.dataInicio = dataInicio
-		self.dataTermino = dataTermino
-		self.pessoa = pessoa
-    
-    def __repr__(self):
-        return '<Setor %r>' % self.nome
-
-class Endereco(Model):
-	__tablename__ = 'enderecos'
-	id = Column(Integer, primary_key = True)
-	logradouro  = Column(String(200))
-	numero  = Column(String(10))
-	complemento  = Column(String(200))
-	bairro  = Column(String(200))
-	municipio  = Column(String(200))
-	pais  = Column(String(100))
-	estado = Column(Enum(UF))
-	cep = Column(String(10))
-	
-	pessoa_id = Column(Integer, ForeignKey('pessoa.id'))
-    pessoa = relationship('Pessoa',  backref=backref('enderecos', lazy='dynamic'))
-	
-	def __init__(self,logradouro, numero, complemento, bairro, municipio, pais, estado, cep, pessoa)
-		self.logradouro = logradouro
-		self.numero = numero
-		self.complemento = complemento
-		self.bairro = bairro
-		self.municipio = municipio
-		self.pais = pais
-		self.estado = estado
-		self.cep = cep
-		self.pessoa = pessoa
-		
-	def __repr__(self):
-        return '<Setor %r>' % self.nome
-	
-class Telefone(Model):
-	__tablename__ = 'telefones'
-	id = Column(Integer, primary_key = True)
-	ddd = Column(String(2))
-	numero = Column(String(12))
-	ramal = Column(String(8))
-	fixo
-	celular
-	prestadora
-	
-	pessoa_id = Column(Integer, ForeignKey('pessoa.id'))
-    pessoa = relationship('Pessoa',  backref=backref('enderecos', lazy='dynamic'))
-	
-	def __init__(self, ddd, numero, ramal, fixo, celular, prestadora):
-        self.ddd = ddd
-        self.numero = numero
-        self.ramal = ramal
-        self.fixo = fixo
-        self.celular = celular
-        self.prestadora = prestadora
-
-    def __repr__(self):
-        return '<Setor %r>' % self.nome
-	
-class Cargo(Model):
-	__tablename__ = 'cargos'
-	id = Column(Integer, primary_key = True)
-	cargo
-	classe
-	padrao
-	dataPosse = Column(DateTime)
-	dataSaida = Column(DateTime)
-	codigoVaga
-	ingresso = Column(DateTime)
-	exercicio = Column(DateTime)
-	
-	pessoa_id = Column(Integer, ForeignKey('pessoa.id'))
-    pessoa = relationship('Pessoa',  backref=backref('enderecos', lazy='dynamic'))
-	
-	def __init__(self, sigla, codigo, nome, dataIngresso, dataSaida, atividade):
-        self.sigla = sigla
-        self.codigo = codigo
-        self.nome = nome
-        self.dataIngresso = dataIngresso
-        self.dataSaida = dataSaida
-        self.atividade = atividade
-
-    def __repr__(self):
-        return '<Setor %r>' % self.nome
-	
-class Funcao(Model):
-	__tablename__ = 'funcoes'
-	id = Column(Integer, primary_key = True)
-	sigla = Column(String(120), unique=True)
-	codigo = Column(String(120), unique=True)
-	nome = Column(String(120), unique=True)
-	dataIngresso = Column(DateTime)
-	dataSaida = Column(DateTime)
-	atividade = Column(String(120), unique=True)
-	
-	pessoa_id = Column(Integer, ForeignKey('pessoa.id'))
-    pessoa = relationship('Pessoa',  backref=backref('enderecos', lazy='dynamic'))
-	
-	def __init__(self, sigla, codigo, nome, dataIngresso, dataSaida, atividade):
-        self.sigla = sigla
-        self.codigo = codigo
-        self.nome = nome
-        self.dataIngresso = dataIngresso
-        self.dataSaida = dataSaida
-        self.atividade = atividade
-
-    def __repr__(self):
-        return '<Setor %r>' % self.nome
-	
-class Setor(Model):
-	__tablename__ = 'setores'
-	id = Column(Integer, primary_key = True)
-	sigla = Column(String(120), unique=True)
-	nome = Column(String(120), unique=True)
-	
-	def __init__(self, sigla, nome):
-        self.sigla = sigla
-        self.nome = nome
-
-    def __repr__(self):
-        return '<Setor %r>' % self.nome
-        
-class Titulos(Model):
-	__tablename__ = 'titulos'
-	id = Column(Integer, primary_key = True)
-	titulo = Column(Enum(Titulacao))
-	area = Column(String(120)) 
-	instituicao = Column(String(120))
-	dataInicio = Column(DateTime)
-	dataTermino = Column(DateTime)
-	
-	pessoa_id = Column(Integer, ForeignKey('Pessoa.id'))
-    pessoa = relationship('Pessoa',  backref=backref('titulos', lazy='dynamic'))
-	
-	def __init__(self, titulo, area, instituicao, dataInicio, dataTermino, pessoa):
-        self.titulo = titulo
-        self.area = area
-        self.instituicao = instituicao
-        self.dataInicio = dataInicio
-        self.dataTermino = dataTermino
-        self.pessoa = pessoa
-
-    def __repr__(self):
-        return '<Setor %r>' % self.nome
-
-
-class TipoServidor(Enum):
+class TipoServidor(enum.Enum):
 	TAE = "Técnico-Administrativo"
 	DOC = "Docente"
 	
-class SituacaoServidor(IntEnum):
-	Ativo = 1
-	Pensionista = 2
-
+class SituacaoServidor(enum.Enum):
+	Ativo = "1"
+	Pensionista = "2"
 
 class Titulacao(enum.Enum):
 	EnsinoBasico = "Ensino Básico e Fundamental"
-    Tecnico = "Técnico"
-    Tecnologo = "Ensino Superior - Tecnologia"
-    Bacharel = "Ensino Superior - Bacharelado"
-    Licenciado = "Ensino Superior - Licenciatura"
-    Especialista = "Pós-Graduação Latu Sensu - Especialização/MBA"
-    Mestre = "Pós-Graduação Strictu Sensu - Mestrado"
-    Doutor = "Pós-Graduação Strictu Sensu - Doutorado"
+	Tecnico = "Técnico"
+	Tecnologo = "Ensino Superior - Tecnologia"
+	Bacharel = "Ensino Superior - Bacharelado"
+	Licenciado = "Ensino Superior - Licenciatura"
+	Especialista = "Pós-Graduação Latu Sensu - Especialização/MBA"
+	Mestre = "Pós-Graduação Strictu Sensu - Mestrado"
+	Doutor = "Pós-Graduação Strictu Sensu - Doutorado"
 
 
 class TipoContaBancaria(enum.Enum):
@@ -296,30 +58,252 @@ class JornadaTrabalho(enum.Enum):
 
 class UF(enum.Enum):
 	AC = "AC"
-    AL = "AL"
-    AM = "AM"
-    AP = "AP"
-    BA = "BA"
-    CE = "CE"
-    DF = "DF"
-    ES = "ES"
-    GO = "GO"
-    MA = "MA"
-    MS = "MS"
-    MG = "MG"
-    MT = "MT"
-    PA = "PA"
-    PB = "PB"
-    PE = "PE"
-    PI = "PI"
-    PR = "PR"
-    RJ = "RJ"
-    RN = "RN"
-    RS = "RS"
-    RO = "RO"
-    RR = "RR"
-    SC = "SC"
-    SE = "SE"
-    SP = "SP"
-    TO = "TO"
+	AL = "AL"
+	AM = "AM"
+	AP = "AP"
+	BA = "BA"
+	CE = "CE"
+	DF = "DF"
+	ES = "ES"
+	GO = "GO"
+	MA = "MA"
+	MS = "MS"
+	MG = "MG"
+	MT = "MT"
+	PA = "PA"
+	PB = "PB"
+	PE = "PE"
+	PI = "PI"
+	PR = "PR"
+	RJ = "RJ"
+	RN = "RN"
+	RS = "RS"
+	RO = "RO"
+	RR = "RR"
+	SC = "SC"
+	SE = "SE"
+	SP = "SP"
+	TO = "TO"
+
+class Pessoa(db.Model):
+	__tablename__ = 'pessoas'
+	id = db.Column(db.Integer, primary_key = True)
+	nome = db.Column(db.String(200), nullable=False)
+	matricula = db.Column(db.String(12), unique=True)
+	matriculaOrigem  = db.Column(db.String(12), unique=True)
+	identificacaoUnica = db.Column(db.String(12), unique=True)
+	tipoServidor = db.Column(db.String(2))
+	situacaoServidor = db.Column(db.String(1))
+	dataCadastroSiape = db.Column(db.DateTime)
+	dataNascimento = db.Column(db.DateTime)
+	estadoCivil  = db.Column(db.String(10))
+	dataPrimeiroEmprego = db.Column(db.DateTime)
+	nacionalidade = db.Column(db.String(200))
+	ufNascimento  = db.Column(db.String(2))
+	nomeMae = db.Column(db.String(200))
+	nomePai = db.Column(db.String(200))
+	naturalidade = db.Column(db.String(200))
+	tipoSanguineo = db.Column(db.String(3))
+	sexo  = db.Column(db.String(1))
+	possuiDeficiencia = db.Column(db.Boolean)
+	TipoDeficiencia = db.Column(db.String(200))
+	raca_cor = db.Column(db.String(200))
+	enderecos = db.relationship("Endereco", back_populates="pessoas")
+	telefones = db.relationship("Telefone", back_populates="pessoas")
+	email_Pessoal = db.Column(db.String(120), unique=True)
+	email_Institucional = db.Column(db.String(120), unique=True)
+	curriculumLattes = db.Column(db.String(120), unique=True)
+	rg_Numero = db.Column(db.String(200))
+	rg_OrgaoExpedidor = db.Column(db.String(200))
+	rg_UF = db.Column(db.String(2))
+	rg_Emissao = db.Column(db.DateTime)
+	cpf = db.Column(db.String(200), unique=True)
+	pis_pasep = db.Column(db.String(20))
+	tituloEleitor = db.Column(db.String(20))
+	tituloEleitor_UF = db.Column(db.String(2))
+	tituloEleitor_Zona = db.Column(db.String(5))
+	tituloEleitor_Secao = db.Column(db.String(5))
+	tituloEleitor_Emissao = db.Column(db.DateTime)
+	certificadoMilitar = db.Column(db.String(20))
+	certificadoMilitar_Orgao = db.Column(db.String(200))
+	certificadoMilitar_Serie = db.Column(db.String(20))
+	passaporte = db.Column(db.String(20))
+	pagamento_Banco = db.Column(db.String(5))
+	pagamento_Agencia = db.Column(db.String(12))
+	pagamento_Conta = db.Column(db.String(12))
+	pagamento_TipoConta = db.Column(db.String(2))
+	cargos = db.relationship("Cargo", back_populates="pessoas")
+	funcoes = db.relationship("Funcao", back_populates="pessoas")
+	titulos = db.relationship("Titulos", back_populates="pessoas")
+	progressoes = db.relationship("Progressao", back_populates="pessoas")
+	
+	jornada = db.Column(db.String(2))
+	
+		
+	def __repr__(self):
+		return '<Pessoa %r>' % self.nome
+			
+class ClasseNivel(db.Model):
+	__tablename__ = 'classesniveis'
+	id = db.Column(db.Integer, primary_key = True)
+	classe = db.Column(db.String(5))
+	nivel = db.Column(db.Integer)
+	
+	def __init__(self,classe,nivel):
+		self.classe = classe
+		self.nivel = nivel
+	
+	def __repr__(self):
+		return '<Classe/Nivel %r>' % self.classe
+
+class Progressao(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	classenivel_id = db.Column(db.Integer, db.ForeignKey('classesniveis.id'))
+	classenivel = db.relationship('ClasseNivel')
+	dataInicio = db.Column(db.DateTime)
+	dataTermino = db.Column(db.DateTime)
+	
+	pessoa_id = db.Column(db.Integer, db.ForeignKey('pessoas.id'))
+	pessoa = db.relationship('Pessoa',  backref=db.backref('progressoes', lazy='dynamic'))
+
+	def __init__(self,classenivel,dataInicio,dataTermino,pessoa):
+		self.classenivel = classenivel
+		self.dataInicio = dataInicio
+		self.dataTermino = dataTermino
+		self.pessoa = pessoa
+	
+	
+class Endereco(db.Model):
+	__tablename__ = 'enderecos'
+	id = db.Column(db.Integer, primary_key = True)
+	logradouro  = db.Column(db.String(200))
+	numero  = db.Column(db.String(10))
+	complemento  = db.Column(db.String(200))
+	bairro  = db.Column(db.String(200))
+	municipio  = db.Column(db.String(200))
+	pais  = db.Column(db.String(100))
+	estado = db.Column(db.String(2))
+	cep = db.Column(db.String(10))
+	
+	pessoa_id = db.Column(db.Integer, db.ForeignKey('pessoas.id'))
+	pessoa = db.relationship('Pessoa',  backref=db.backref('enderecos', lazy='dynamic'))
+	
+	def __init__(self,logradouro, numero, complemento, bairro, municipio, pais, estado, cep, pessoa):
+		self.logradouro = logradouro
+		self.numero = numero
+		self.complemento = complemento
+		self.bairro = bairro
+		self.municipio = municipio
+		self.pais = pais
+		self.estado = estado
+		self.cep = cep
+		self.pessoa = pessoa
+	
+class Telefone(db.Model):
+	__tablename__ = 'telefones'
+	id = db.Column(db.Integer, primary_key = True)
+	ddd = db.Column(db.String(2))
+	numero = db.Column(db.String(12))
+	ramal = db.Column(db.String(8))
+	fixo = db.Column(db.Boolean)
+	celular = db.Column(db.Boolean)
+	prestadora = db.Column(db.String(12))
+	
+	pessoa_id = db.Column(db.Integer, db.ForeignKey('pessoas.id'))
+	pessoa = db.relationship('Pessoa',  backref=db.backref('enderecos', lazy='dynamic'))
+	
+	def __init__(self, ddd, numero, ramal, fixo, celular, prestadora):
+		self.ddd = ddd
+		self.numero = numero
+		self.ramal = ramal
+		self.fixo = fixo
+		self.celular = celular
+		self.prestadora = prestadora
+
+	
+class Cargo(db.Model):
+	__tablename__ = 'cargos'
+	id = db.Column(db.Integer, primary_key = True)
+	cargo  = db.Column(db.String(200))
+	classe   = db.Column(db.String(200))
+	padrao   = db.Column(db.String(200))
+	dataPosse = db.Column(db.DateTime)
+	dataSaida = db.Column(db.DateTime)
+	codigoVaga  = db.Column(db.String(200))
+	ingresso = db.Column(db.DateTime)
+	exercicio = db.Column(db.DateTime)
+	
+	pessoa_id = db.Column(db.Integer, db.ForeignKey('pessoas.id'))
+	pessoa = db.relationship('Pessoa',  backref=db.backref('enderecos', lazy='dynamic'))
+	
+	
+class Funcao(db.Model):
+	__tablename__ = 'funcoes'
+	id = db.Column(db.Integer, primary_key = True)
+	sigla = db.Column(db.String(120), unique=True)
+	codigo = db.Column(db.String(120), unique=True)
+	nome = db.Column(db.String(120), unique=True)
+	dataIngresso = db.Column(db.DateTime)
+	dataSaida = db.Column(db.DateTime)
+	atividade = db.Column(db.String(120), unique=True)
+	
+	pessoa_id = db.Column(db.Integer, db.ForeignKey('pessoas.id'))
+	pessoa = db.relationship('Pessoa',  backref=db.backref('enderecos', lazy='dynamic'))
+	
+	def __init__(self, sigla, codigo, nome, dataIngresso, dataSaida, atividade):
+		self.sigla = sigla
+		self.codigo = codigo
+		self.nome = nome
+		self.dataIngresso = dataIngresso
+		self.dataSaida = dataSaida
+		self.atividade = atividade
+
+	
+class Campus(db.Model):
+	__tablename__ = 'campus'
+	id = db.Column(db.Integer, primary_key = True)
+	sigla = db.Column(db.String(120), unique=True)
+	nome = db.Column(db.String(120), unique=True)
+	
+	def __init__(self, sigla, nome):
+		self.sigla = sigla
+		self.nome = nome
+
+
+class Setor(db.Model):
+	__tablename__ = 'setores'
+	id = db.Column(db.Integer, primary_key = True)
+	sigla = db.Column(db.String(120), unique=True)
+	nome = db.Column(db.String(120), unique=True)
+	
+	def __init__(self, sigla, nome):
+		self.sigla = sigla
+		self.nome = nome
+
+	def __repr__(self):
+		return '<Setor %r>' % self.nome
+
+		
+class Titulo(db.Model):
+	__tablename__ = 'titulos'
+	id = db.Column(db.Integer, primary_key = True)
+	titulo = db.Column(db.String(20))
+	area = db.Column(db.String(120)) 
+	instituicao = db.Column(db.String(120))
+	dataInicio = db.Column(db.DateTime)
+	dataTermino = db.Column(db.DateTime)
+	
+	pessoa_id = db.Column(db.Integer, db.ForeignKey('pessoas.id'))
+	pessoa = db.relationship('Pessoa',  backref=db.backref('titulos', lazy='dynamic'))
+	
+	def __init__(self, titulo, area, instituicao, dataInicio, dataTermino, pessoa):
+		self.titulo = titulo
+		self.area = area
+		self.instituicao = instituicao
+		self.dataInicio = dataInicio
+		self.dataTermino = dataTermino
+		self.pessoa = pessoa
+
+	def __repr__(self):
+		return '<Titulo %r>' % self.titulo
 
